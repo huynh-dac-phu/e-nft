89a7cb5b550fb7147f49a19d17b99468997d46ee
phuhd-0935 <huynh.dac.phu@sun-asterisk.com>
2025-06-21 21:33:33 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello Tailwind!</h1>
+      <p className="mt-2 text-gray-600">Vite + React + TS + Tailwind is ready ✅</p>
+    </div>
+
+      <a href="/dash">x dash</a>
+    </>
+  )
+}
+
+export default App
